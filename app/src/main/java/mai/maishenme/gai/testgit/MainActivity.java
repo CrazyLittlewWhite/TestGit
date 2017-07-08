f26587638c4be288047e1347a3a4641ff973a282
PengRui <18773703117@163.com>
2017-07-08 11:39:50 +0800

    aa

--- app/src/main/java/mai/maishenme/gai/testgit/MainActivity.java
+++ app/src/main/java/mai/maishenme/gai/testgit/MainActivity.java
@@ -2,13 +2,16 @@ package mai.maishenme.gai.testgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
     }
 }
